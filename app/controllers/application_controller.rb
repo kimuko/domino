091bb91376e9089de21ba0a63af571535296405f
chanibarin <ysfactory.ibarin@gmail.com>
2018-07-31 14:03:55 +0900

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options(options = {})
-    #return { locale: "" } unless params[:locale]
+    return options unless params[:locale]
     { locale: I18n.locale }.merge options
   end
 end
